809998bd85290a363d0488820d81d761cb3d2e0b
Domenico Baratta <barattadom@gmail.com>
2024-01-17 17:29:30 +0100

    Add project files.

---
+++ ItemsReworked/ItemsReworkedPlugin.cs
@@ -0,0 +1,42 @@
+﻿#region usings
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+#endregion
+
+namespace ItemsReworked
+{
+    [BepInPlugin("ItemsReworkedPlugin", "ItemsReworkedPlugin", "1.0.0")]
+
+    public class ItemsReworkedPlugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("ItemsReworkedPlugin");
+        internal static ItemsReworkedPlugin Instance { get; private set; }
+        internal static ManualLogSource mls;
+
+        #region Config
+        /// <summary>
+        /// Config - Example
+        /// </summary>
+        #region BrackenSettings
+        internal static ConfigEntry<bool> ConfigEntry;
+        #endregion
+
+        #endregion
+
+        void Awake()
+        {
+            #region Configs
+            //TO BE ADDED
+            #endregion
+            Instance = this;
+            mls = BepInEx.Logging.Logger.CreateLogSource("Loading Items Reworked Plugin");
+
+            harmony.PatchAll();
+
+
+            mls.LogInfo("Items Reworked Plugin loaded sucessfully.");
+        }
+    }
+}
---
+++ ItemsReworked/Patches/RemotePropPatches.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace ItemsReworked.Patches
+{
+    [HarmonyPatch(typeof(RemoteProp))]
+    internal class RemotePropPatches
+    {
+        [HarmonyPatch("ItemActivate")]
+        [HarmonyPostfix]
+        private static void UseRemote(RemoteProp __instance, bool used, bool buttonDown = true)
+        {
+            TriggerLandMines(__instance);
+        }
+
+        private static bool TriggerLandMines(RemoteProp remote)
+        {
+            var localPlayer = StartOfRound.Instance.localPlayerController;
+            var playersFace = localPlayer.gameplayCamera.transform.position;
+            var facingDirection = localPlayer.gameplayCamera.transform.forward;
+
+            // Maximum distance for the infection spreadDistance
+            float spreadDistance = 10f;
+
+            // Casting a ray in looking direction
+            var ray = Physics.RaycastAll(playersFace, facingDirection, spreadDistance, 1, QueryTriggerInteraction.Ignore);
+
+            foreach (var hit in ray)
+            {
+                Landmine Landmine = hit.collider.GetComponent<Landmine>();
+
+                //Avoid detecting self
+                if (Landmine != null)
+                {
+                    Landmine.Detonate();
+                }
+            }
+
+            return true;
+        }
+    }
+}
